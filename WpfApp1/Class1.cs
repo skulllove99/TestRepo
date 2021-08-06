d93b2015bd743e6c39c79f76d690efcb05e77619
Truong Minh <skulllove131299@gmail.com>
2021-08-06 17:46:40 +0700

    day la class 1

---
+++ WpfApp1/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Class1
+    {
+        // Đây là class 1
+    }
+}
